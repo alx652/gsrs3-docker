b556a0ae9b4d591a9ac15880681ebce4c1035826
alx652 <welscha2@nih.gov>
2024-12-02 12:22:59 -0500

    refine scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
+_SUDO=''
+PROJECT=${PROJECT:-hocon}
+PROGRESS=${PROGRESS:-'--progress=plain'}
+CACHE=${CACHE:-'--no-cache'}
+DEFAULT_IMAGE_NAME=gsrs3i-${PROJECT}:latest
+IMAGE_NAME=${IMAGE_NAME:-$DEFAULT_IMAGE_NAME}
 
-PROJECT=hocon
-PROGRESS='--progress=plain'
-CACHE='--no-cache'
-IMAGE_NAME=${gsrs3i-${PROJECT}:latest}
-
-
-docker build -f Dockerfile.${PROJECT}.devel --ulimit nofile=65535:65535 $CACHE $PROGRESS  -t "${IMAGE_NAME}" . 
+$_SUDO docker build -f Dockerfile.${PROJECT}.devel --ulimit nofile=65535:65535 $CACHE $PROGRESS  -t "${IMAGE_NAME}" . 
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 # Default variable assignment in bash
 # https://unix.stackexchange.com/questions/122845/using-a-b-for-variable-assignment-in-scripts/122848#122848
-
+$_SUDO=''
 PROJECT="${PROJECT:-hocon}"
 IMAGE_NAME='gsrs3i-${PROJECT}:latest'
-
 DEFAULT_LOCAL_VOLUME=$(cd .. && pwd)/volumens/v1$PROJECT
 LOCAL_VOLUME="${LOCAL_VOLUME:-$DEFAULT_LOCAL_VOLUME}"
 VOLUME=$LOCAL_VOLUME':/home/srs'
@@ -14,4 +13,4 @@ CONTAINER_NAME=gsrs3c-${PROJECT}
 # Tip: use this to copy frontend config to running container. 
 # docker cp frontend_config.json $CONTAINER_NAME:/usr/local/tomcat/webapps/frontend/WEB-INF/classes/static/assets/data/config.json
 
-docker run --name $CONTAINER_NAME -d -p 8080:8080 -v $VOLUME -e CATALINA_OPTS='-Xms20g -Xmx20g -XX:ReservedCodeCacheSize=512m' -e DB_DDL_AUTO=$DB_DDL_AUTO $IMAGE_NAME
+$_SUDO docker run --name $CONTAINER_NAME -d -p 8080:8080 -v $VOLUME -e CATALINA_OPTS='-Xms20g -Xmx20g -XX:ReservedCodeCacheSize=512m' -e DB_DDL_AUTO=$DB_DDL_AUTO $IMAGE_NAME
